86fd7729f97ff7b1e41652990df08e4da231d8f5
jofrepinheiro <jofrepinheiro@gmail.com>
2016-03-23 13:16:00 -0300

    Importing Project Configurations

---
+++ Processador/Barramento.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Processador
+{
+    class Barramento
+    {
+        private int dado;
+
+        /*   A única função do Barramento é a de repassar dados
+         *   Logo, duas simples funções.
+         *   Uma para receber, outra para enviar o dado.
+         */
+
+        public int receber() { return dado; }
+        public void enviar(int dado) { this.dado = dado; }
+    }
+}
